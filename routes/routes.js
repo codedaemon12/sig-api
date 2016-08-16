b4256466b26796a35f9224bc808b5e91002ebd39
System Administrator <bhaskarjyoti86@gmail.com>
2016-08-15 23:00:10 -0700

    first commit

---
+++ routes/routes.js
@@ -0,0 +1,43 @@
+var appRouter = function(app) {
+    app.get("/", function(req, res) {
+      res.send("Hello World");
+  });
+  app.get("/account", function(req, res) {
+      var accountMock = {
+          "username": "nraboy",
+          "password": "1234",
+          "twitter": "@nraboy"
+      }
+      if(!req.query.username) {
+          return res.send({"status": "error", "message": "missing username"});
+      } else if(req.query.username != accountMock.username) {
+          return res.send({"status": "error", "message": "wrong username"});
+      } else {
+          return res.send(accountMock);
+      }
+  });
+  app.post("/account", function(req, res) {
+      if(!req.body.username || !req.body.password) {
+          return res.send({"status": "error", "message": "missing a parameter"});
+      } else {
+          return res.send({
+            "message" :"nice job"
+          });
+      }
+});
+  app.post("/verify", function(req, res) {
+      console.log(req.body.SAMLResponse);
+      return res.send({
+        "message" :"nice job"
+      });
+      // if(!req.body.username || !req.body.password) {
+      //     return res.send({"status": "error", "message": "missing a parameter"});
+      // } else {
+      //     return res.send({
+      //       "message" :"nice job"
+      //     });
+      // }
+  });
+}
+
+module.exports = appRouter;
